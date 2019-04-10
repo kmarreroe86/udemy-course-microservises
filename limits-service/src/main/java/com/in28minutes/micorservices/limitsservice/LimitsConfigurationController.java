6aae8fa51543f3debe18bb268e6a9c480ec09d37
karel <kmarreroe86@gmail.com>
2019-04-09 23:27:42 -0500

    Add Hystrix for fault tolerance

--- limits-service/src/main/java/com/in28minutes/micorservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/in28minutes/micorservices/limitsservice/LimitsConfigurationController.java
@@ -5,17 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.micorservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
-	
+
 	@Autowired
 	private Configuration configuration;
-	
+
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
-		
+
 		return new LimitConfiguration(configuration.getMinimun(), configuration.getMaximun());
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfigurations")
+	public LimitConfiguration retrieveConfigurations() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfigurations() {
+		return new LimitConfiguration(9, 99);
 	}
 
 }
